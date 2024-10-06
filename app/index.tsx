73ca1fe9560e27deafd24cbeea08f74150c376c9
Supriyo maji <122034818+supriyo010@users.noreply.github.com>
2024-10-06 17:04:35 +0530

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const index = () => {
+  return (
+    <View>
+      <Text>index</Text>
+      <Button title='signup'></Button>
+    </View>
+  )
+}
+
+export default index
+
+const styles = StyleSheet.create({})
